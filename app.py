b1668dcb382744b1d02742a093e9b6bd1ff07743
frdanwrhdyt <frdanwrhdyt@gmail.com>
2023-04-11 14:40:35 +0700

    lc

--- app.py
+++ app.py
@@ -6,57 +6,59 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 cors = CORS(app)
 
-# Load the GeoJSON data into a GeoDataFrame
 desa = gpd.read_file('static/data.geojson')
 bts = gpd.read_file('static/bts.geojson')
-# uso = gdp.read_file('static/uso.geojson')
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
+uso = gpd.read_file('static/uso.geojson')
+sl = gpd.read_file('static/sl.geojson')
 
 @app.route('/desa')
-# @cross_origin()
 def desaGeojson():
-    # Parse the bounding box coordinates from the request query parameters
     xmin = float(request.args.get('xmin'))
     ymin = float(request.args.get('ymin'))
     xmax = float(request.args.get('xmax'))
     ymax = float(request.args.get('ymax'))
 
-    # Create a bounding box geometry from the coordinates
     bbox = box(xmin, ymin, xmax, ymax)
 
-    # Filter the GeoDataFrame to select only features that intersect with the bounding box
     filtered_gdf = desa[desa.geometry.intersects(bbox)]
 
-    # Convert the filtered GeoDataFrame to GeoJSON format and return it as a response
     response_data = filtered_gdf.to_json()
     response = jsonify(response_data)
     response.headers.add('Content-Type', 'application/json')
 
     return response
 @app.route('/bts')
-# @cross_origin()
 def btsGeojson():
-    # Parse the bounding box coordinates from the request query parameters
     xmin = float(request.args.get('xmin'))
     ymin = float(request.args.get('ymin'))
     xmax = float(request.args.get('xmax'))
     ymax = float(request.args.get('ymax'))
 
-    # Create a bounding box geometry from the coordinates
     bbox = box(xmin, ymin, xmax, ymax)
 
-    # Filter the GeoDataFrame to select only features that intersect with the bounding box
     filtered_gdf = bts[bts.geometry.intersects(bbox)]
 
-    # Convert the filtered GeoDataFrame to GeoJSON format and return it as a response
     response_data = filtered_gdf.to_json()
     response = jsonify(response_data)
     response.headers.add('Content-Type', 'application/json')
 
     return response
 
+@app.route('/uso')
+def usoGeojson():
+    response_data = uso.to_json()
+    response = jsonify(response_data)
+    response.headers.add('Content-Type', 'application/json')
+
+    return response
+
+@app.route('/sl')
+def slGeojson():
+    response_data = sl.to_json()
+    response = jsonify(response_data)
+    response.headers.add('Content-Type', 'application/json')
+
+    return response
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(debug=True, port=5050, host='0.0.0.0')
